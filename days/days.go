6d64930bd8d9a5f0eb266680cabb71810c7a48e8
shaeselix <shae.selix@gmail.com>
2020-12-06 12:56:44 -0800

    day 6 + days

---
+++ days/days.go
@@ -0,0 +1,29 @@
+package days
+
+import (
+	"fmt"
+	"aoc_2020/days/day1"
+	"aoc_2020/days/day2"
+	"aoc_2020/days/day3"
+	"aoc_2020/days/day4"
+	"aoc_2020/days/day5"
+	"aoc_2020/days/day6"
+)
+
+var (
+	dataFileFormat string = "%d.txt"
+	DayFns = map[int]func(string) {
+		1: day1.Execute,
+		2: day2.Execute,
+		3: day3.Execute,
+		4: day4.Execute,
+		5: day5.Execute,
+		6: day6.Execute,
+	}
+)
+
+func ExecuteDay(day int, datadir string) {
+	file := fmt.Sprintf(dataFileFormat, day)
+	fp := fmt.Sprintf("%s%s", datadir, file)
+	DayFns[day](fp)
+}
